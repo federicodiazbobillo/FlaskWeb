2a700c6aa8f0e15ec32b1ff6a0e96bca0158c491
Federico Diaz Bobillo <claro_regio@hotmail.com>
2024-07-12 17:41:25 -0300

    Agregamos el config choto

--- FlaskWeb/__init__.py
+++ FlaskWeb/__init__.py
@@ -6,3 +6,23 @@ from flask import Flask
 app = Flask(__name__)
 
 import FlaskWeb.views
+
+
+# app/__init__.py
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app import routes, models
+    return app
